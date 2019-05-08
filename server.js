c8836b6b1f9a57c58af07faedfa631beabeb85e7
mateus.cardoso <mateus.cardoso@basis.com.br>
2019-05-08 14:30:49 -0300

    projeto tst socket IO

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const server = require('http').createServer(app)
+const io = require('socket.io')(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use('/', (req, res) => {
+    res.send('index.html');
+})
+
+let msg = [];
+
+io.on('connection', socket => {
+    console.log(`Socket conectado: ${socket.id}`)
+
+    socket.emit('msgAnterios', msg);
+
+    socket.on('tstProJeffersonVer', data => {
+        msg.push(data);
+        socket.broadcast.emit('valorRecebido', data);
+    })
+})
+
+server.listen(3000)
